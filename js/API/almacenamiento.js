a5a4e5ee73bb91edc5ee50cfdce9d078a00475fe
Carlos Alberto Rodriguez Garcia <tacubayo8@gmail.com>
2014-02-27 00:08:33 -0600

    ProyectoHuevo
    a huevo

---
+++ js/API/almacenamiento.js
@@ -0,0 +1,28 @@
+//Almacenamiento
+function altaUsario(){
+    window.localStorage.setItem('usuario',$('#regNom').val());
+}
+
+function usuarioExiste(){
+    if(window.localStorage.getItem('usuario')!=undefined)
+        return true;
+    else
+        return false;
+}
+
+function accesoBD(){
+    var db = window.openDatabase("hotel", "1.0", "Hotal BD", 2000000);
+    return db;
+}
+
+function guardarReserva(th,pr,ha,di){
+    alert(th);
+    accesoBD().transaction(function(tx){
+        tx.executeSql('CREATE TABLE IF NOT EXISTS reservas (id unique,th,pr,ha,di)');
+        tx.executeSql('INSERT INTO reservas (th,pr,ha,di) VALUES ("'+th+'","'+pr+'","'+ha+'","'+di+'")');
+    },function(err){
+        alert('Error: '+err.code);
+    },function(){
+        navigator.notification.alert('Esperando Conexión a Internet',null,'Datos Guardados','Aceptar');
+    });
+}
